8a1dd98d9abc72656d63c916bc1d81246f6b86d3
thomas-anderson-0313 <thomasanderson19960313@gmail.com>
2023-01-26 02:06:55 +1000

    added:new_third_span_page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import First_SP from './pages/1_sp';
 import Second_EN from './pages/2_en';
 import Second_SP from "./pages/2_sp";
 import Third_EN from './pages/3_en';
+import Third_SP from './pages/3_sp';
 import Forth_EN from './pages/4_en';
 
 import Visits from "./pages/visits";
@@ -21,6 +22,7 @@ function App() {
           <Route path = "/engaca2" element = {<Second_EN />} />
           <Route path = "/spanaca2" element = {<Second_SP />} />
           <Route path = "/engmed1" element = {<Third_EN />} />
+          <Route path = "/spanmed1" element = {<Third_SP />} />
           <Route path = "/hbosolar" element = {<Forth_EN />} />
 
           <Route path = "/daily-america-savings-view" element = {<Visits />} />
