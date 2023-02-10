434fc8784ad989763ebfe8b05155a3dbfb24ca11
thomas-anderson-0313 <thomasanderson19960313@gmail.com>
2023-02-10 22:46:30 +1000

    changed:heading_engerc1

--- src/pages/5_en.tsx
+++ src/pages/5_en.tsx
@@ -272,7 +272,7 @@ export default function Fifth_EN() {
 				<>
 				<div className='main-container-5'>
 					<div className='main-descrition-5'>
-						<div className='main-des-title'>Business Owners Nationwide are getting FREE Stimulus Check of upto $26,000 per employee under this Congress-Approved Program! Check if you're eligible to receive your Stimulus Check, Here's how!</div>
+						<div className='main-des-title'>Millions of Business Owners Nationwide are getting FREE Stimulus Check of upto $26,000 per employee under this Congress-Approved Program. Here's how!</div>
 						{/* <img src = {Head_img} alt = "head" width = "100%" /> */}
 						<img className='topic-img-5' src = {Head_bg} alt = "head"/>
 						<div className='main-des-5'>Americans Business owners can qualify for a Free Stimulus Check of upto $26,000 per employee (that they kept on payroll in 2020 and or 2021). This is not a loan, this is Free Money that business owners can use in anything they wish.</div>
