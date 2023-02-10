a027e4c2c4f368f32a596d448543fc4d5aed6d8c
thomas-anderson-0313 <thomasanderson19960313@gmail.com>
2023-02-10 23:02:16 +1000

    removed:brackets

--- src/pages/5_en.tsx
+++ src/pages/5_en.tsx
@@ -275,7 +275,7 @@ export default function Fifth_EN() {
 						<div className='main-des-title'>Millions of Business Owners Nationwide are getting<span style={{backgroundColor:"#fde047"}}> FREE Stimulus Check of upto $26,000 per employee</span> under this Congress-Approved Program. Here's how!</div>
 						{/* <img src = {Head_img} alt = "head" width = "100%" /> */}
 						<img className='topic-img-middle' src = {Head_bg} alt = "head"/>
-						<div className='main-des-5'>Americans Business owners can qualify for a Free Stimulus Check of upto $26,000 per employee (that they kept on payroll in 2020 and or 2021). This is not a loan, this is Free Money that business owners can use in anything they wish.</div>
+						<div className='main-des-5'>Americans Business owners can qualify for a Free Stimulus Check of upto $26,000 per employee that they kept on payroll in 2020 and or 2021. This is not a loan, this is Free Money that business owners can use in anything they wish.</div>
 						<div className='main-des-5' style = {{marginTop:"1rem"}}><b>The opportunity to claim your Stimulus Check</b> (if eligible) <b>ends on March 31st</b> so it's best to check your eligibility now - its as easy as answering the 3 questions below:</div>
 					</div>
 					<div className='survey'>
