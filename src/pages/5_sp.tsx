585a7c098b0fc16416163eee757d77a5bd75d292
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2023-09-13 06:27:03 +0530

    mic check

--- src/pages/5_sp.tsx
+++ src/pages/5_sp.tsx
@@ -244,7 +244,7 @@ export default function Fifth_SP() {
 
     return(
         <div>
-			<div className='top-sticky-blue' id='top'>Emergency Relief Programs</div>
+			<div className='top-sticky-blue' id='top'>Emergency Relief Program</div>
 			{step==="process"?
 				<>
 					<div className='main-container-5'>
