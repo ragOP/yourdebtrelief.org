a01a1eecd8b4cc1cfb7197b43398526686439720
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-04-05 05:25:26 +0530

    Update 5_sp.tsx

--- src/pages/5_sp.tsx
+++ src/pages/5_sp.tsx
@@ -309,9 +309,9 @@ useEffect(() => {
 						<div className='top-description-5'>¡Haga una llamada rápida para reclamar su alivio de deuda de hasta el 100 % ahora!</div>
 						<div className='spots-count'>Lugares restantes: 4</div>
 						<div className='tap-direction'>👇TOCA ABAJO PARA LLAMAR👇</div>
-						<a href = "tel:+18335141750">
+						<a href = "tel:+18553050321>
 							<div className='call-btn' onClick={handleCall}>
-							CALL (833) 514-1750
+							CALL (855) 305-0321
 							</div>
 						</a>
 						<div className='sub-title'>Nosotras hemos reservado tu lugar</div>
